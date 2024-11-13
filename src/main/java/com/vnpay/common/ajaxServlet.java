b60d50f47323bb6786ac14411612774125c76c31
Sunshower1260 <155897796+Sunshower1260@users.noreply.github.com>
2024-11-13 09:08:08 +0700

    Update ajaxServlet.java

--- src/main/java/com/vnpay/common/ajaxServlet.java
+++ src/main/java/com/vnpay/common/ajaxServlet.java
@@ -5,15 +5,18 @@
  */
 package com.vnpay.common;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.IOException;import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.TimeZone;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -24,67 +27,86 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author CTT VNPAY
  */
-public class vnpayQuery extends HttpServlet {
+public class ajaxServlet extends HttpServlet {
 
     @Override
-
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //Command:querydr
-
-        String vnp_RequestId = Config.getRandomNumber(8);
-        String vnp_Version = "2.1.0";
-        String vnp_Command = "querydr";
-        String vnp_TmnCode = Config.vnp_TmnCode;
-        String vnp_TxnRef = req.getParameter("order_id");
-        String vnp_OrderInfo = "Kiem tra ket qua GD OrderId:" + vnp_TxnRef;
-        //String vnp_TransactionNo = req.getParameter("transactionNo");
-        String vnp_TransDate = req.getParameter("trans_date");
         
+        String vnp_Version = "2.1.0";
+        String vnp_Command = "pay";
+        String orderType = "other";
+        long amount = Integer.parseInt(req.getParameter("amount"))*100;
+        String bankCode = req.getParameter("bankCode");
+        String vnp_TxnRef = req.getParameter("orderId");
+        String vnp_IpAddr = Config.getIpAddress(req);
+
+        String vnp_TmnCode = Config.vnp_TmnCode;
+        
+        Map<String, String>  vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+        
+        if (bankCode != null && !bankCode.isEmpty()) {
+            vnp_Params.put("vnp_BankCode", bankCode);
+        }
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_OrderType", orderType);
+
+        String locate = req.getParameter("language");
+        if (locate != null && !locate.isEmpty()) {
+            vnp_Params.put("vnp_Locale", locate);
+        } else {
+            vnp_Params.put("vnp_Locale", "vn");
+        }
+        vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
         
-        String vnp_IpAddr = Config.getIpAddress(req);
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
         
-        JsonObject  vnp_Params = new JsonObject ();
-        
-        vnp_Params.addProperty("vnp_RequestId", vnp_RequestId);
-        vnp_Params.addProperty("vnp_Version", vnp_Version);
-        vnp_Params.addProperty("vnp_Command", vnp_Command);
-        vnp_Params.addProperty("vnp_TmnCode", vnp_TmnCode);
-        vnp_Params.addProperty("vnp_TxnRef", vnp_TxnRef);
-        vnp_Params.addProperty("vnp_OrderInfo", vnp_OrderInfo);
-        //vnp_Params.put("vnp_TransactionNo", vnp_TransactionNo);
-        vnp_Params.addProperty("vnp_TransactionDate", vnp_TransDate);
-        vnp_Params.addProperty("vnp_CreateDate", vnp_CreateDate);
-        vnp_Params.addProperty("vnp_IpAddr", vnp_IpAddr);
-        
-        String hash_Data= String.join("|", vnp_RequestId, vnp_Version, vnp_Command, vnp_TmnCode, vnp_TxnRef, vnp_TransDate, vnp_CreateDate, vnp_IpAddr, vnp_OrderInfo);
-        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hash_Data.toString());
-        
-        vnp_Params.addProperty("vnp_SecureHash", vnp_SecureHash);
-        
-        URL url = new URL (Config.vnp_ApiUrl);
-        HttpURLConnection con = (HttpURLConnection)url.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(vnp_Params.toString());
-        wr.flush();
-        wr.close();
-        int responseCode = con.getResponseCode();
-        System.out.println("nSending 'POST' request to URL : " + url);
-        System.out.println("Post Data : " + vnp_Params);
-        System.out.println("Response Code : " + responseCode);
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(con.getInputStream()));
-        String output;
-        StringBuffer response = new StringBuffer();
-        while ((output = in.readLine()) != null) {
-        response.append(output);
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
         }
-        in.close();
-        System.out.println(response.toString());
+        String queryUrl = query.toString();
+        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+        com.google.gson.JsonObject job = new JsonObject();
+        job.addProperty("code", "00");
+        job.addProperty("message", "success");
+        job.addProperty("data", paymentUrl);
+        Gson gson = new Gson();
+        resp.getWriter().write(gson.toJson(job));
     }
+
 }
